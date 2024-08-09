5e54c5e2fb0317d1df6a1a5ff6705e1403cfe6d0
Scolar02 <njugunascolar83@gmail.com>
2024-08-09 09:57:58 +0000

    pep 8

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Deletion-resilient hypermedia pagination
 """
-
 import csv
-from typing import Dict, List, Optional
+from typing import Dict, List
 
 
 class Server:
@@ -33,48 +32,34 @@ class Server:
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
                 i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Retrieves info about a page from a given index and with a
         specified size.
         """
-        assert isinstance(
-            index, int) and index >= 0, "Index must be a non-negative integer"
-        assert isinstance(
-            page_size, int) and page_size > 0, "Page size must be a positive integer"
-
         data = self.indexed_dataset()
-        sorted_indexes = sorted(data.keys())
-
-        # If index is out of the dataset bounds
-        if index not in sorted_indexes:
-            return {
-                'index': index,
-                'next_index': None,
-                'page_size': 0,
-                'data': []
-            }
-
-        # Collect the requested page data
+        assert index is not None and index >= 0 and index <= max(data.keys())
         page_data = []
-        next_index = None
         data_count = 0
-
-        for i in sorted_indexes:
-            if i >= index and data_count < page_size:
-                page_data.append(data[i])
+        next_index = None
+        start = index if index else 0
+        for i, item in data.items():
+            if i >= start and data_count < page_size:
+                page_data.append(item)
                 data_count += 1
-                next_index = i + 1
-            elif data_count >= page_size:
+                continue
+            if data_count == page_size:
+                next_index = i
                 break
-
-        return {
+        page_info = {
             'index': index,
             'next_index': next_index,
             'page_size': len(page_data),
             'data': page_data,
         }
+        return page_info
